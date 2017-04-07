8bf176e6d49ca0c9c206137a2aea1d95399c084f
unknown <saidkh@gmail.com>
2017-04-07 23:24:46 +0300

    Add PersonsTableSeeder

---
+++ database/seeds/PersonsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Person;
+use Illuminate\Database\Seeder;
+
+class PersonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        foreach (range(1,10) as $index) {
+            Person::create([
+                'first_name' => $faker->firstName($gender = null|'male'|'female') ,
+                'second_name' => $faker->firstNameMale ,
+                'third_name' => $faker->firstNameMale ,
+                'last_name' => $faker->lastName ,
+                //'full_name' => $faker->name ,
+
+                'birth_date' => $faker->date($format = 'Y-m-d', $max = 'now') ,
+
+                'national_id' => $faker->unique()->randomNumber($nbDigits = 9, $strict = false) ,
+
+            ]);
+        }
+    }
+}
